00d0775c63dbcfbae005df45726fc2b2e7818797
Don Drake <don@drakeconsulting.com>
2016-11-17 17:46:48 -0600

    fix main class, pass through command-line vals

--- runApp.sh
+++ runApp.sh
@@ -4,5 +4,5 @@ SRCDIR=$( cd -P $(dirname ${BASH_SOURCE[0]}); echo $PWD )
 
 spark-submit \
     --master local[*] \
-    --class com.drakeconsulting.SimpleApp \
-    $SRCDIR/target/scala-2.10/BigDataMaker-assembly-1.0.jar
+    --class com.drakeconsulting.big_data_maker.SimpleApp \
+    $SRCDIR/target/scala-2.10/BigDataMaker-assembly-1.0.jar $*
