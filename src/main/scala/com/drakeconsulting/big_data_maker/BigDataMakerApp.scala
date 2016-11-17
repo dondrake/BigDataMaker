b0dbd1e360c0bfd28aaba7d5bccd386254de24ee
Don Drake <don@drakeconsulting.com>
2016-11-17 17:49:20 -0600

    first example file

--- src/main/scala/com/drakeconsulting/big_data_maker/BigDataMakerApp.scala
+++ src/main/scala/com/drakeconsulting/big_data_maker/BigDataMakerApp.scala
@@ -22,15 +22,42 @@ package com.drakeconsulting.big_data_maker
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+import java.io.File
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
+    case class Config(outDir: File = new File("."))
+    val parser = new scopt.OptionParser[Config]("Big Data Maker") {
+      head("Big Data Maker", "1.0")
+      opt[File]('o', "outDir").required.action( (x,c) => c.copy(outDir = x) ).text("outDir is a directory")
+    }
+
+    var outDir = ""
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        outDir = config.outDir.toString
+
+      case None =>
+        println("Oops.")
+        System.exit(1)
+    }
+
+    println("outDir=" + outDir)
+    val conf = new SparkConf().setAppName("Big Data Maker Application")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    val sqlContext = new SQLContext(sc)
+
+    val bigData = new BigData(sqlContext, outDir, 10, 1000)
+    bigData.addColumn(new StringConstant("f1", "testing"))
+    bigData.addColumn(new RandomLong("f2", 100000000000L))
+    bigData.addColumn(new RandomLong("f3", 10000000000L))
+    bigData.addColumn(new RandomLong("f4", 1000000000L))
+    bigData.addColumn(new RandomDouble("f5", 1000000000.0))
+    bigData.addColumn(new RandomDouble("f6", 100000000.0))
+    bigData.addColumn(new RandomDouble("f7", 10000000.0))
+
+    bigData.writeFile
+
   }
 }
