865a759beb90ccb16290576587e1ebf91c54ef7b
Drake <drakedon@8c85906ee9a3.ant.amazon.com>
2019-11-24 10:46:40 -0600

    upgrade

--- runApp.sh
+++ runApp.sh
@@ -6,4 +6,4 @@ spark-submit \
     --master local[*] \
     --driver-memory 8G \
     --class com.drakeconsulting.big_data_maker.SimpleApp \
-    $SRCDIR/target/scala-2.10/BigDataMaker-assembly-1.0.jar $*
+    $SRCDIR/target/scala-2.11/BigDataMaker-assembly-1.0.jar $*
